24cdb9f487620bd34af73127d65289955a6520f2
Adriana Soares Couto <a106869@uminho.pt>
2024-11-09 15:47:39 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,15 +1,14 @@
 import typer
 import requests #pede acesso ao api
 from typing import Optional
-from typing_extensions import Annotated
 
 API_KEY = '71c6f8366ef375e8b61b33a56a2ce9d9'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', #engana o api a pensar que estou a aceder pro um navegador
 }
 
-def response(): #função para fazer a requisição
-    url = f'https://api.itjobs.pt/job/list.json?api_key={API_KEY}'
+def response(page): #função para fazer a requisição
+    url = f'https://api.itjobs.pt/job/list.json?api_key={API_KEY}&page={page}'
     response = requests.get(url, headers=headers)
     data = response.json()
     return data
@@ -23,7 +22,7 @@ def top(n: int, csv: bool = False):
     jobs = []
     page = 1
     while len(jobs) < n:
-        data = response()
+        data = response(page)
         jobs += data['results']
         page += 1
         if not data['results']: 
@@ -50,51 +49,45 @@ def top(n: int, csv: bool = False):
         #titulo;empresa;descricao;data de publicacao;salario;localizacao
     #else:
         #break
-#kshbcksjbcslj
+
 @app.command()
-def search(nome: str, localidade: str, n: Annotated[Optional[int], typer.Argument()]=None, csv: bool = False):
-    """ Lista todos os trabalhos full-time publicados por uma determminada empresa, numa determinada região """
-    data = response()
-    data = data['results']
-    jobs_full_time = (data['company']['name']== nome and data['types'][0]["name"]== "Full-time" and data['locations']==['locations'][0]['name']== localidade)
+def search(nome: str, localidade: str, n: Optional[int] = None, csv: bool = False):
+    """ Lista todos os trabalhos full-time publicados por uma determminada empresa, numa determinada região. Insira o nome da empresa e da localidade entre aspas para melhor funcionamento. """
+    jobs_full_time = []
+    page = 1
+    while True:
+        data = response(page)
+        for job in data['results']: 
+            company_name = job.get('company',{}).get('name', None) #dicionário então {}; valor então None
+            if company_name == nome:
+                types = job.get('types',[{}])  #lista de dicionários então [{}]
+                if types[0].get('name') == 'Full-time':
+                    locations = job.get('locations', [{}]) #lista de dicionários então  [{}]
+                    if any(location.get('name', None) == localidade for location in locations): #valor então None
+                        jobs_full_time.append(job)
+        page += 1
+        if 'results' not in data:
+            break
     if n is not None:
         jobs_full_time = jobs_full_time[:n]
-        for x in jobs_full_time:
-            title = x.get('title', 'NA') #se não existir valor em 'title' apresenta 'NA'
-            company_name = x.get('company', {}).get('name', 'NA')
-            body = x.get('body', 'NA')
-            published_at = x.get('publishedAt', 'NA')
-            try: #tenta aceder ao index 0 da lista, se não existir retorna 'NA'
-                location = x['locations'][0].get('name', 'NA') 
-            except (IndexError, KeyError): 
-                location = 'NA'
-            wage = x.get('wage', 'NA')
-            print(f"Título: {title}")
-            print(f"Empresa: {company_name}")
-            print(f"Description: {body}")
-            print(f"Data de publicação: {published_at}")
-            print(f"Location: {location}")
-            print(f"Salário: {wage}")
+    for x in jobs_full_time:
+        title = x.get('title', 'NA') #se não existir valor em 'title' apresenta 'NA'
+        company_name = x.get('company', {}).get('name', 'NA')
+        body = x.get('body', 'NA')
+        published_at = x.get('publishedAt', 'NA')
+        try: #tenta aceder ao index 0 da lista, se não existir retorna 'NA'
+            location = x['locations'][0].get('name', 'NA') 
+        except (IndexError, KeyError): 
+            location = 'NA'
+        wage = x.get('wage', 'NA')
+        print(f"Título: {title}")
+        print(f"Empresa: {company_name}")
+        print(f"Description: {body}")
+        print(f"Data de publicação: {published_at}")
+        print(f"Location: {location}")
+        print(f"Salário: {wage}")
 
-    else:
-        for x in jobs_full_time:
-            title = x.get('title', 'NA') #se não existir valor em 'title' apresenta 'NA'
-            company_name = x.get('company', {}).get('name', 'NA')
-            body = x.get('body', 'NA')
-            published_at = x.get('publishedAt', 'NA')
-            try: #tenta aceder ao index 0 da lista, se não existir retorna 'NA'
-                location = x['locations'][0].get('name', 'NA') 
-            except (IndexError, KeyError): 
-                location = 'NA'
-            wage = x.get('wage', 'NA')
-            print(f"Título: {title}")
-            print(f"Empresa: {company_name}")
-            print(f"Description: {body}")
-            print(f"Data de publicação: {published_at}")
-            print(f"Location: {location}")
-            print(f"Salário: {wage}")
-
-
+#if csv:
     #tem que permitir inserir o número de traablhos a apresentar, caso contrário apresenta todos os trabalhos
     #argumento opcional para csv
 
